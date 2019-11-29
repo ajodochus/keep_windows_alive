a98060e34a0ff1e9a37e2a08c2d22baa7d536676
bernd.wissmeier <bernd.wissmeier@stp-online.de>
2019-11-29 08:43:42 +0100

    final

--- WpfApp1/Properties/AssemblyInfo.cs
+++ WpfApp1/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("WpfApp1")]
+[assembly: AssemblyTitle("KDO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApp1")]
+[assembly: AssemblyProduct("KDO")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
